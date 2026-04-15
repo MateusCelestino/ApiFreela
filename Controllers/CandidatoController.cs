c168a3b46aaece3790e76d2e5c567bb35c27970b
Mateus Celestino Delleteze <130002471+MateusCelestino@users.noreply.github.com>
2026-04-15 20:04:18 -0300

    Inicial

---
+++ Controllers/CandidatoController.cs
@@ -0,0 +1,65 @@
+﻿using ApiProjetoVagas.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjetoCandidato.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CandidatoController : ControllerBase
+    {
+        private List<Candidato> PegarDados()
+        {
+            // Criar uma lista de Alunos vazia para receber os dados do arquivo
+            List<Candidato> listaCandidato = new();
+
+            try
+            {
+                // Pegar arquivo c:\temp\candidato.json e trazer para a memória
+                string dadosArquivo = System.IO.File.ReadAllText("c:\\temp\\candidato.json");
+                listaCandidato = System.Text.Json.JsonSerializer.Deserialize<List<Candidato>>(dadosArquivo);
+            }
+            catch { }
+
+            return listaCandidato;
+        }
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            return Ok(PegarDados());
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Candidato vaga)
+        {
+            List<Candidato> listaCandidato = PegarDados();
+            listaCandidato.Add(vaga);
+            SalvarArquivo(listaCandidato);
+            return Ok(PegarDados());
+        }
+        [HttpDelete]
+
+        public IActionResult Deletar(int id)
+        {
+            List<Candidato> listaCandidato = PegarDados();
+            SalvarArquivo(listaCandidato.Where(item => item.Id != id).ToList());
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult Editar(Candidato vaga)
+        {
+            var listaCandidato = PegarDados();
+            listaCandidato = listaCandidato.Where((item => item.Id != vaga.Id)).ToList();
+            listaCandidato.Add(vaga);
+            SalvarArquivo(listaCandidato);
+            return Ok();
+        }
+        private void SalvarArquivo(List<Candidato> listaCandidato)
+        {
+            string dadosJson = System.Text.Json.JsonSerializer.Serialize(listaCandidato);
+            System.IO.File.WriteAllText("c:\\temp\\candidato.json", dadosJson);
+
+        }
+    }
+}
---
+++ Models/Candidato.cs
@@ -0,0 +1,15 @@
+﻿namespace ApiProjetoVagas.Models
+{
+    public class Candidato:Logar
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Sobrenome { get; set; }
+        public string Experiencia { get; set; } 
+        public string email { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Telefone { get; set; }
+        public Decimal valor { get; set; }
+
+    }
+}
