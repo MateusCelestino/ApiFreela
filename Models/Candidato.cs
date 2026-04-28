6057f6727f63321d8a11d934bbabaa34a55d35a3
Mateus Celestino Delleteze <130002471+MateusCelestino@users.noreply.github.com>
2026-04-28 20:11:54 -0300

    projeto

--- Models/Candidato.cs
+++ Models/Candidato.cs
@@ -1,15 +1,26 @@
-﻿namespace ApiProjetoVagas.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiProjetoVagas.Models
 {
-    public class Candidato:Logar
+    public class Candidato : Logar
     {
         public int Id { get; set; }
 
+        [Required]
         public string Nome { get; set; }
-        public string Sobrenome { get; set; }
-        public string Experiencia { get; set; }
-        public DateTime DataNascimento { get; set; }
-        public string Telefone { get; set; }
-        public Decimal valor { get; set; }
 
+        [Required]
+        public string Sobrenome { get; set; }
+
+        public string Experiencia { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{10,11}$",
+            ErrorMessage = "Telefone deve conter apenas números (DDD + número)")]
+        public string Telefone { get; set; }
+
+        public decimal Valor { get; set; }
     }
 }
